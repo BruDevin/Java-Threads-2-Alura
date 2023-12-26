a4121656ed81cb8c467f2ea79ac41128be3d3f97
BruDevin <bruno.ads.2022@gmail.com>
2023-12-26 19:19:50 -0300

    Projeto do curso: Cliente + Servidor

---
+++ src/servidor/TratadorDeExcecao.java
@@ -0,0 +1,10 @@
+package servidor;
+
+public class TratadorDeExcecao implements Thread.UncaughtExceptionHandler {
+
+    @Override
+    public void uncaughtException(Thread t, Throwable e) {
+        System.out.println("Deu exceção na thread " + t.getName() + ", " + e.getMessage());
+    }
+
+}
